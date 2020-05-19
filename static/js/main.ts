4395446ccc58b9fe2cbcbcdff90fa4d493ae129e
bjellesma <william.jellesma@gmail.com>
2020-05-19 15:29:38 -0400

    remove connect method

--- static/js/main.ts
+++ static/js/main.ts
@@ -17,7 +17,7 @@ if(document.getElementById('chatRoom')){
     ignoreQueryPrefix: true
   });
   //@ts-ignore
-  const socket = io.connect('wss://chatchord.herokuapp.com', {transports: ['websocket']});
+  const socket = io()
 
   // Join chatroom
   socket.on('connect', function(){
